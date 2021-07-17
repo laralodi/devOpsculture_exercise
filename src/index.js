3e54726c970a60000ea3f17de3e997383422db75
laralodi <85138892+laralodi@users.noreply.github.com>
2021-07-17 11:35:49 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World do Nosso tutorial');
+  res.status(200).send('Se você chegou até aqui, você concluiu o exercicio. Parabéns!');
 });
 
 module.exports = app;
